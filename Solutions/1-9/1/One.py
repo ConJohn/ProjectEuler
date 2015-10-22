ed2139e8927e0b871ba5f84902e7e267646a2e1f
Jake Conway <jake.conway@sbcglobal.net>
2015-10-22 12:55:49 -0500

    Create One.py

---
+++ Solutions/1-9/1/One.py
@@ -0,0 +1,9 @@
+class One:
+  def main():
+    total = 0;
+    for x in range(0,1000):
+      if x%3 == 0 or x%5 == 0:
+        total += x
+    print("Solution: " + str(total))
+if __name__ == '__main__':
+  One.main()
